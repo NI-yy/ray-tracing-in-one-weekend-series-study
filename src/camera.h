8ce873b9575a4650d31ef785fd315ef662e366c4
NI-yy <iy.nozomy@gmail.com>
2026-02-04 19:20:07 +0900

    chapter3

--- src/camera.h
+++ src/camera.h
@@ -33,10 +33,13 @@ class camera {
 
 				for (int i = 0; i < image_width; i++) {
 					color pixel_color(0, 0, 0);
-					for (int sample = 0; sample < samples_per_pixel; sample++) {
-						ray r = get_ray(i, j);
-						pixel_color += ray_color(r, max_depth, world);
+					for (int s_j = 0; s_j < sqrt_spp; s_j++) {
+						for (int s_i = 0; s_i < sqrt_spp; s_i++) {
+							ray r = get_ray(i, j, s_i, s_j);
+							pixel_color += ray_color(r, max_depth, world);
+						}
 					}
+
 					write_color(image_file, pixel_samples_scale * pixel_color);
 				}
 			}
@@ -48,6 +51,8 @@ class camera {
 	private:
 		int image_height;
 		double pixel_samples_scale; // Color scale factor for a sum of pixel samples
+		int sqrt_spp; // Square root of number of samples per pixel
+		double recip_sqrt_spp; // 1 / sqrt_spp
 		point3 center;
 		point3 pixel00_loc;
 		vec3 pixel_delta_u;
@@ -61,7 +66,9 @@ class camera {
 			image_height = int(image_width / aspect_ratio);
 			image_height = (image_height < 1) ? 1 : image_height;
 
-			pixel_samples_scale = 1.0 / samples_per_pixel;
+			sqrt_spp = int(std::sqrt(samples_per_pixel));
+			pixel_samples_scale = 1.0 / (sqrt_spp * sqrt_spp);
+			recip_sqrt_spp = 1.0 / sqrt_spp;
 
 			center = lookfrom;
 
@@ -93,11 +100,13 @@ class camera {
 			defocus_disk_v = v * defocus_radius;
 		}
 
-		ray get_ray(int i, int j) const {
-			// Construct a camera ray originating from the defocus disk and directed at randomly samlpled
-			// point around the pixel location i, j.
+		ray get_ray(int i, int j, int s_i, int s_j) const {
+			// Construct a camera ray originating from the defocus disk and directed at a randomly
+			// sampled point around the pixel location i, j for stratified sample square s_i, s_j.
 
-			auto offset = sample_square();
+
+			auto offset = sample_square_stratified(s_i, s_j);
+
 			auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
 			auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
@@ -106,6 +115,14 @@ class camera {
 
 			// Considering the time up to the shutter closed, we randomly sample time(0 to 1).
 			return ray(ray_origin, ray_direction, ray_time);
+		}
+
+		vec3 sample_square_stratified(int s_i, int s_j) const {
+			// Returns the vector to a random point in the square sub-pixel specified by grid
+			// indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+			auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+			auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+			return vec3(px, py, 0);
 		}
 
 		vec3 sample_square() const {
--- src/main.cpp
+++ src/main.cpp
@@ -190,6 +190,7 @@ void cornell_box() {
     auto green = make_shared<lambertian>(color(.12, .45, .15));
     auto light = make_shared<diffuse_light>(color(15, 15, 15));
 
+    // Cornell box sides
     world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), green));
     world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), red));
     world.add(make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), light));
@@ -197,10 +198,15 @@ void cornell_box() {
     world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
     world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
 
+    // Light
     shared_ptr<hittable> box1 = box(point3(0, 0, 0), point3(165, 330, 165), white);
+
+    // Box1
     box1 = make_shared<rotate_y>(box1, 15);
     box1 = make_shared<translate>(box1, vec3(265, 0, 295));
     world.add(box1);
+
+    // Box2
     shared_ptr<hittable> box2 = box(point3(0, 0, 0), point3(165, 165, 165), white);
     box2 = make_shared<rotate_y>(box2, -18);
     box2 = make_shared<translate>(box2, vec3(130, 0, 65));
@@ -328,9 +334,13 @@ void final_scene(int image_width, int samples_per_pixel, int max_depth) {
     cam.render(world);
 }
 
+void cornellBox_revisited() {
+
+}
+
 
 int main() {
-    switch (9) {
+    switch (7) {
         case 1: bouncing_spheres(); break;
         case 2: checkered_spheres(); break;
         case 3: earth(); break;
